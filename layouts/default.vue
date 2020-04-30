9f14f77077e78c2eb43e2027e8f36e0e5880c659
ilhamanugrah13 <ilhamxanugrah@gmail.com>
2020-04-30 14:12:15 +0700

    add project

---
+++ layouts/default.vue
@@ -0,0 +1,179 @@
+<template>
+  <div>
+    <nav
+      class="navbar header"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href="/"
+        >
+          dazzl
+        </a>
+
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <b-navbar-item href="#">
+            Women
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Men
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Kids
+          </b-navbar-item>
+          <b-navbar-item class="cart" href="#" />
+          <b-navbar-item href="#">
+            Login
+          </b-navbar-item>
+        </div>
+      </div>
+    </nav>
+    <nuxt />
+    <div class="footer">
+      <div class="columns">
+        <div class="column is-6">
+          <ul class="footer-nav">
+            <li><a href="">about us</a></li>
+            <li><a href="">find your store</a></li>
+          </ul>
+        </div>
+        <div class="column is-6">
+          <div class="newsletter">
+            <b-field>
+              <b-input
+                custom-class="newsletter-input"
+                placeholder="Enter your email address"
+                type="email"
+              />
+              <p class="control">
+                <button class="button is-primary">
+                  Subscribe
+                </button>
+              </p>
+            </b-field>
+          </div>
+        </div>
+      </div>
+      <p class="has-text-centered">
+        &copy; dazzl 2020
+      </p>
+    </div>
+    <!-- <section class="main-content columns">
+      <aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
+        <ul class="menu-list">
+          <li
+            v-for="(item, key) of items"
+            :key="key"
+          >
+            <nuxt-link
+              :to="item.to"
+              exact-active-class="is-active"
+            >
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar-brand {
+  .navbar-item {
+    font-family: "Monserrat", sans-serif;
+    font-size: 32px;
+
+    &:hover {
+      color: pink;
+    }
+  }
+}
+
+.navbar-end {
+  .navbar-item:hover {
+    background-color: black;
+    color: white;
+  }
+}
+
+.cart {
+  background-image: url('~@/assets/img/icons/supermarket.png');
+  background-repeat: no-repeat;
+  padding: 8px 30px;
+  width: 20px;
+  background-size:20px;
+  background-position: center;
+
+  &:hover {
+    background-image: url('~@/assets/img/icons/supermarket-white.png');
+  }
+}
+
+.footer {
+  background: black;
+  color: white;
+}
+
+.footer-nav {
+  li {
+    a {
+      color: white;
+
+      &:hover {
+        color: pink;
+      }
+    }
+  }
+}
+
+.newsletter {
+  .newsletter-input {
+    border-radius: 0;
+  }
+
+  .button {
+    border-radius: 0;
+    background: #ccc;
+    color: black;
+    font-family: "Monserrat", sans-serif;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,180 @@
+<template>
+  <section>
+    <div class="columns is-centered slider-layout">
+      <b-carousel
+        :autoplay="false"
+        class="column is-8"
+      >
+        <b-carousel-item v-for="(carousel, i) in carousels" :key="i">
+          <img
+            :src="require(`@/assets/img/banner/${i+1}.png`)"
+            :alt="`banner${i}`"
+            class="slider-image"
+          >
+          <!-- <section :class="`hero is-medium is-${carousel.color}`">
+            <div class="hero-body has-text-centered">
+              <h1 class="title">
+                {{ carousel.text }}
+              </h1>
+            </div>
+          </section> -->
+        </b-carousel-item>
+        <div slot="indicators" class="slider-indicator" />
+      </b-carousel>
+    </div>
+    <div class="section-brand">
+      <h2 class="heading-section">
+        shop by brand
+      </h2>
+      <div class="slider-brand">
+        <b-carousel-list
+          :data="sliderBrand.items"
+          has-drag="drag"
+          repeat="true"
+          items-to-list="4"
+        >
+          <img
+            slot="item"
+            slot-scope="props"
+            :src="require(`@/assets/img/brand/${props.list.image}.png`)"
+            :alt="props.list.name"
+            class="image-brand"
+          >
+        </b-carousel-list>
+      </div>
+    </div>
+    <div class="section-shop">
+      <h2 class="heading-section">
+        latest arrival
+      </h2>
+      <div class="slider-latest">
+        <b-carousel-list
+          :data="sliderLatest.items"
+          has-drag="drag"
+          repeat="true"
+          items-to-show="4"
+        >
+          <ui-card
+            slot="item"
+            slot-scope="props"
+            :title="props.list.name"
+            :image="props.list.image"
+            :price="props.list.price"
+          />
+        </b-carousel-list>
+      </div>
+    </div>
+  </section>
+  <!-- <section>
+    <div class="columns is-mobile" />
+  </section> -->
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+  components: {
+    UiCard: Card
+  },
+  data () {
+    return {
+      carousels: [
+        { text: 'Slide 1', color: 'primary' },
+        { text: 'Slide 2', color: 'info' },
+        { text: 'Slide 3', color: 'success' }
+      ],
+      sliderBrand: {
+        items: [
+          {
+            name: 'Bershka',
+            image: 'bershka'
+          },
+          {
+            name: 'Pull & Bear',
+            image: 'pnb'
+          },
+          {
+            name: 'Seibu',
+            image: 'seibu'
+          },
+          {
+            name: 'Sogo',
+            image: 'sogo'
+          },
+          {
+            name: 'Zara',
+            image: 'zara'
+          }
+        ]
+      },
+      sliderLatest: {
+        items: [
+          {
+            name: 'Women Luxury Dress',
+            image: 'latest1.jpg',
+            price: 'IDR580.000'
+          },
+          {
+            name: 'Man Khaki shirt',
+            image: 'latest2.jpg',
+            price: 'IDR720.000'
+          },
+          {
+            name: 'Black dazzl dress',
+            image: 'latest3.jpg',
+            price: 'IDR999.000'
+          },
+          {
+            name: 'Slim-fit gray',
+            image: 'latest4.jpg',
+            price: 'IDR220.000'
+          },
+          {
+            name: 'White Cycle Sweatshirt Kid',
+            image: 'latest5.jpg',
+            price: 'IDR250.000'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel-list {
+  box-shadow: none;
+}
+
+.slider-layout {
+  .indicator-item {
+    &.is-active {
+      .slider-indicator {
+        background: white;
+      }
+    }
+
+    .slider-indicator {
+      border-radius: 10px;
+      height: 10px;
+      width: 10px;
+      background: black;
+      border: 0;
+      margin-bottom: 20px;
+    }
+  }
+
+  .slider-image {
+    width: 100%;
+  }
+
+}
+
+.slider-brand {
+  .carousel-list {
+    text-align: center;
+  }
+}
+</style>
